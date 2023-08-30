99776af174c81c206674b1c319302b6f82361709
Heer Kirov <HeerKirov@outlook.com>
2023-08-30 18:13:59 +0800

    feat(frontend/dialog): 调整对话框可自适应高度

--- frontend/src/components-module/dialog/GlobalDialog.vue
+++ frontend/src/components-module/dialog/GlobalDialog.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { ref, computed, ComponentPublicInstance } from "vue"
 import { DialogBox } from "@/components/interaction"
+import { useElementRect, useWindowSize } from "@/utils/sensors"
 import {
     useInternalService,
     SourceDataEditorProps, MetaTagEditorProps, CreatingCollectionProps, CreatingBookProps,
@@ -19,29 +21,44 @@ import ExternalExporter from "./ExternalExporter/ExternalExporter.vue"
 
 const { context, close } = useInternalService()
 
+const innerRef = ref<ComponentPublicInstance>()
+
+const innerElement = computed<HTMLElement>(() => innerRef.value?.$el)
+
+const innerElementRect = useElementRect(innerElement)
+
+const innerElementHeight = computed(() => innerElementRect.value?.height)
+
+const windowSize = useWindowSize()
+
+const thresholdHeight = computed(() => windowSize.value.height * 0.75 - 20) //magic number 20：是0.5rem+2px的边界值，再加2px的冗余。减去这个数字才是内容区域的边界阈值
+
+const fixed = computed(() => innerElementHeight.value && innerElementHeight.value >= thresholdHeight.value)
+
 </script>
 
 <template>
-    <DialogBox :class="$style['container-box']" :visible="context !== null" @close="close" :close-on-click-outside="true" :close-on-escape="true">
-        <SourceDataEditor v-if="context!.type === 'sourceDataEditor'" :p="context!.props as SourceDataEditorProps" @close="close"/>
-        <MetaTagEditor v-else-if="context!.type === 'metaTagEditor'" :p="context!.props as MetaTagEditorProps" @close="close"/>
-        <CreatingCollection v-else-if="context!.type === 'creatingCollection'" :p="context!.props as CreatingCollectionProps" @close="close"/>
-        <CreatingBook v-else-if="context!.type === 'creatingBook'" :p="context!.props as CreatingBookProps" @close="close"/>
-        <AddIllust v-else-if="context!.type === 'addIllust'" :p="context!.props as AddIllustProps" @close="close"/>
-        <AddToFolder v-else-if="context!.type === 'addToFolder'" :p="context!.props as AddToFolderProps" @close="close"/>
-        <CloneImage v-else-if="context!.type === 'cloneImage'" :p="context!.props as CloneImageProps" @close="close"/>
-        <FindSimilarTaskExplorer v-else-if="context!.type === 'findSimilarTaskExplorer'" :p="(context!.props as FindSimilarTaskExplorerProps)" @close="close"/>
-        <AssociateExplorer v-else-if="context!.type === 'associateExplorer'" :p="(context!.props as AssociateExplorerProps)" @close="close"/>
-        <ExternalExporter v-else-if="context!.type === 'externalExporter'" :p="(context!.props as ExternalExporterProps)" @close="close"/>
+    <DialogBox :class="{[$style['container-box']]: true, [$style.fixed]: fixed}" :visible="context !== null" @close="close" :close-on-click-outside="true" :close-on-escape="true">
+        <SourceDataEditor v-if="context!.type === 'sourceDataEditor'" ref="innerRef" :p="context!.props as SourceDataEditorProps" @close="close"/>
+        <MetaTagEditor v-else-if="context!.type === 'metaTagEditor'" ref="innerRef" :p="context!.props as MetaTagEditorProps" @close="close"/>
+        <CreatingCollection v-else-if="context!.type === 'creatingCollection'" ref="innerRef" :p="context!.props as CreatingCollectionProps" @close="close"/>
+        <CreatingBook v-else-if="context!.type === 'creatingBook'" ref="innerRef" :p="context!.props as CreatingBookProps" @close="close"/>
+        <AddIllust v-else-if="context!.type === 'addIllust'" ref="innerRef" :p="context!.props as AddIllustProps" @close="close"/>
+        <AddToFolder v-else-if="context!.type === 'addToFolder'" ref="innerRef" :p="context!.props as AddToFolderProps" @close="close"/>
+        <CloneImage v-else-if="context!.type === 'cloneImage'" ref="innerRef" :p="context!.props as CloneImageProps" @close="close"/>
+        <FindSimilarTaskExplorer v-else-if="context!.type === 'findSimilarTaskExplorer'" ref="innerRef" :p="(context!.props as FindSimilarTaskExplorerProps)" @close="close"/>
+        <AssociateExplorer v-else-if="context!.type === 'associateExplorer'" ref="innerRef" :p="(context!.props as AssociateExplorerProps)" @close="close"/>
+        <ExternalExporter v-else-if="context!.type === 'externalExporter'" ref="innerRef" :p="(context!.props as ExternalExporterProps)" @close="close"/>
     </DialogBox>
 </template>
 
 <style module lang="sass">
 .container-box
     padding: 0.5rem
-    // min-height: 10vh
-    // max-height: 80vh
-    height: 70vh
+    min-height: 10vh
+    max-height: 75vh
+    &.fixed
+        height: 75vh
     width: 95vw
     @media screen and (min-width: 800px)
         width: 720px
--- frontend/src/utils/sensors.ts
+++ frontend/src/utils/sensors.ts
@@ -3,22 +3,22 @@ import { sleep } from "@/utils/process"
 
 /**
  * 提供一个observer，监视一个Element的Resize事件。
- * @param ref 引用此Element的ref
+ * @param elementRef 引用此Element的ref
  * @param event 事件
  */
-export function onElementResize(ref: Ref<HTMLElement | undefined>, event: (rect: DOMRect) => void) {
+export function onElementResize(elementRef: Ref<HTMLElement | undefined>, event: (rect: DOMRect) => void) {
     let element: HTMLElement | undefined = undefined
     const observer = new ResizeObserver(entries => event(entries[0].contentRect))
 
     onMounted(() => {
-        if(ref.value) observer.observe(element = ref.value)
+        if(elementRef.value) observer.observe(element = elementRef.value)
     })
 
     onUnmounted(() => {
         if(element) observer.unobserve(element)
     })
 
-    watch(ref, v => {
+    watch(elementRef, v => {
         if(element) observer.unobserve(element)
         if((element = v) != undefined) observer.observe(element)
     })
@@ -129,4 +129,48 @@ export function useResizeBar(options: {areaRef: Ref<HTMLElement | undefined>, lo
     })
 
     return {resizeAreaMouseDown}
+}
+
+/**
+ * 获得窗口尺寸的响应式数据。
+ */
+export function useWindowSize() {
+    const windowSize = ref({height: window.innerHeight, width: window.innerWidth})
+    
+    const windowResized = () => {
+        windowSize.value = {height: window.innerHeight, width: window.innerWidth}
+    }
+
+    onMounted(() => {
+        window.addEventListener("resize", windowResized)
+    })
+
+    onUnmounted(() => {
+        window.removeEventListener("resize", windowResized)
+    })
+
+    return windowSize
+}
+
+/**
+ * 获得元素尺寸和方位的响应式数据。
+ */
+export function useElementRect(elementRef: Ref<HTMLElement | undefined>) {
+    const rect = ref<DOMRect>()
+
+    onMounted(() => {
+        if(elementRef.value) {
+            rect.value = elementRef.value.getBoundingClientRect()
+        }
+    })
+
+    watch(elementRef, ele => {
+        rect.value = ele?.getBoundingClientRect()
+    })
+
+    onElementResize(elementRef, value => {
+        rect.value = value
+    })
+
+    return rect
 }
