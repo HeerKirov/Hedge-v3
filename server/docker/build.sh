969b056a5a0d9d4828291d11691ba8009be1b940
Heer Kirov <HeerKirov@outlook.com>
2024-11-11 22:10:22 +0800

    feat(server/docker): 更新docker脚本

--- server/docker/build.sh
+++ server/docker/build.sh
@@ -2,8 +2,9 @@
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 TAG=${1:-dev}
+FILE=${2:-$SCRIPT_DIR/hedge-v3-server.jar}
 
-if [ ! -f "$SCRIPT_DIR/hedge-v3-server.jar" ]; then
+if [ ! -f "$FILE" ]; then
     echo "hedge-v3-server.jar not found."
     exit 1
 fi
@@ -12,4 +13,6 @@ if [ ! -x "$SCRIPT_DIR/start.sh" ]; then
     chmod +x $SCRIPT_DIR/start.sh
 fi
 
-sudo docker build $SCRIPT_DIR -t hedge-v3-server:$TAG
+cp $FILE $SCRIPT_DIR/hedge-v3-server.jar
+docker build $SCRIPT_DIR -t hedge-v3-server:$TAG
+rm $SCRIPT_DIR/hedge-v3-server.jar
