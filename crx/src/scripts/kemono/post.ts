a9693d799317d1f7c7c30262fcc74b2839c8ab9e
Heer Kirov <HeerKirov@outlook.com>
2024-11-30 19:40:23 +0800

    fix(crx/scripts): 修复kemono站点变更导致的content script失效

--- crx/src/scripts/kemono/post.ts
+++ crx/src/scripts/kemono/post.ts
@@ -8,31 +8,61 @@ import { onDOMContentLoaded } from "@/utils/document"
 import { Result } from "@/utils/primitives"
 
 onDOMContentLoaded(() => {
-    console.log("[Hedge v3 Helper] kemono/post script loaded.")
-    const sourcePath = getSourceDataPath()
-    if(sourcePath !== null) {
-        const sourceData = collectSourceData()
-        console.log(sourcePath, sourceData)
-        sendMessage("SUBMIT_PAGE_INFO", {path: sourcePath})
-        sendMessage("SUBMIT_SOURCE_DATA", {path: sourcePath, data: sourceData})
+    function observeMainInitialize(callback: () => void) {
+        if(document.querySelector("main") && document.querySelector("meta[name=\"published\"]")) {
+            callback()
+            return
+        }
+        const ready = {main: false, metaTime: false}
+        const observer = new MutationObserver(mutationsList => {
+            for(const mutation of mutationsList) {
+                for(const addedNode of mutation.addedNodes) {
+                    if(addedNode instanceof Element && addedNode.querySelector("main")) {
+                        ready.main = true
+                    }else if(addedNode instanceof HTMLMetaElement && addedNode.name === "published") {
+                        ready.metaTime = true
+                    }
+                    if(ready.metaTime && ready.main) {
+                        callback()
+                        observer.disconnect()
+                        return
+                    }
+                }
+            }
+        })
+        observer.observe(document, { childList: true, subtree: true })
     }
 
-    initializeUI(sourcePath)
+    observeMainInitialize(async () => {
+        console.log("[Hedge v3 Helper] kemono/post script loaded.")
+        const sourcePath = getSourceDataPath()
+        if(sourcePath !== null) {
+            const sourceData = await collectSourceData()
+            console.log(sourcePath, sourceData)
+            sendMessage("SUBMIT_PAGE_INFO", {path: sourcePath})
+            sendMessage("SUBMIT_SOURCE_DATA", {path: sourcePath, data: sourceData})
+        }
+
+        initializeUI(sourcePath)
+    })
+
 })
 
 receiveMessageForTab(({ type, msg: _, callback }) => {
     if(type === "REPORT_SOURCE_DATA") {
-        callback(collectSourceData())
+        collectSourceData().then(r => callback(r))
+        return true
     }else if(type === "REPORT_PAGE_INFO") {
         callback({path: getSourceDataPath()})
     }else if(type === "QUICK_FIND_SIMILAR") {
         const sourceDataPath = getSourceDataPath()
         if(sourceDataPath !== null) {
-            const sourceData = collectSourceData()
-            const file = document.querySelector<HTMLImageElement>("a#image-link img#image")
-            similarFinder.quickFind(file?.src, sourceDataPath, sourceData)
+            collectSourceData().then(sourceData => {
+                const file = document.querySelector<HTMLImageElement>("a#image-link img#image")
+                similarFinder.quickFind(file?.src, sourceDataPath, sourceData)
+            })
+            return true
         }
-        return false
     }
     return false
 })
@@ -55,14 +85,10 @@ function initializeUI(sourceDataPath: SourceDataPath | null) {
 /**
  * 收集来源数据。
  */
-function collectSourceData(): Result<SourceDataUpdateForm, string> {
+async function collectSourceData(): Promise<Result<SourceDataUpdateForm, string>> {
+    const { uid } = getIdentityInfo()
     const tags: SourceTagForm[] = []
 
-    //添加uid作为tag。在目前支持的site(fanbox, fantia, patreon, gumroad)中，所有site都将uid存储为artist类型。
-    const uid = document.querySelector<HTMLMetaElement>("meta[name=\"user\"]")?.content
-    if(!uid) {
-        return {ok: false, err: `Cannot find meta[name=user].`}
-    }
     tags.push({code: uid, type: "artist"})
 
     const titleSpan = document.querySelector<HTMLSpanElement>(".post__title > span:first-child")
